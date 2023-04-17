0844335588fc6c5539d00e483834f811ab4536fe
IvanyutinSA <124494631+IvanyutinSA@users.noreply.github.com>
2023-04-17 19:20:19 +0600

    Create MathEventArgs.cs

---
+++ Semester 2/Algorithmization/Aud Labs/Lab_7/MathEventArgs.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casual
+{
+    internal class MathEventArgs : EventArgs
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+}
